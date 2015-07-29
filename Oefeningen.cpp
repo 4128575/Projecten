a024568a1fe7c69c3d58d9c791d31dbb03c95a54
a4128575 <richard.sad@hotmail.com>
2015-07-29 09:46:07 +0000

    Oefening gemaakt Cels to Fahr

--- Oefeningen.cpp
+++ Oefeningen.cpp
@@ -2,13 +2,48 @@
 #include <cmath>
 using namespace std;
 
-int main(){
-    int a, b;
-    a=5;
-    b=2;
-    a=a+1;
-    int result;
-    result = a-b;
-    cout << result << endl;
+int main()
+{
+    double low=0;
+    double high=50000;
+    
+    double lowlim;
+    cout << "Please enter a lower limit: ";
+    cin >> lowlim;
+    while (lowlim<low){
+        cout << "Lower limit should be higher than " << low << ", give a new one: ";
+        cin >> lowlim;
+    }
+    
+    double highlim;
+    cout << "Please enter a higher limit: ";
+    cin >> highlim;
+    while (highlim>high){
+        cout << "Higher limit should be lower than " << high << ", give a new one: ";
+        cin >> highlim;
+    }
+    while (highlim<lowlim){
+        cout << "Higher limit should be higher than " << lowlim << ", give a new one: ";
+        cin >> highlim;
+    }
+    
+    double step;
+    cout << "Please enter a step size: ";
+    cin >> step;
+    while (step<=0){
+        cout << "Step size should be greater than 0, give a new one: ";
+        cin >> step;
+    }
+    while (step>(highlim-lowlim)){
+        cout << "Step size should be smaller than difference in temperature, " << highlim-lowlim << ", give a new one: ";
+        cin >> step;
+    }
+    
+    cout << "Celsius    |Fahrenheit \n---------------------- \n";
+    int n=0;
+    while (lowlim + n*step < highlim) {
+        cout << lowlim + n*step << "         |" << (lowlim + n*step)*1.8 + 32 << "\n";
+        ++n;
+    }
     return 0;
 }
