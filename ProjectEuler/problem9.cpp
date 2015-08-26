204d1689a9429a647627a528b2c4d498cccc1e9e
a4128575 <richard.sad@hotmail.com>
2015-08-26 17:23:14 +0000

    PrEu prob 9 af

---
+++ ProjectEuler/problem9.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <sstream>
+#include <vector>
+#include <string>
+#include <cmath>
+using namespace std;
+
+int main(){
+    int a,b,c;
+    int ans=0;
+    for (a=1;a<1000;a++){
+        for (b=1;b<1000;b++){
+            for (c=1;c<1000;c++){
+                if (a+b+c==1000){
+                    if((a*a)+(b*b)==(c*c)) {
+                        ans=a*b*c;
+                        break;
+                    }
+                }
+            }
+        }
+    }
+    cout << ans << endl;
+}
