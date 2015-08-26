88f7362634d0ee2c91fda6a2504f06828c3ed323
a4128575 <richard.sad@hotmail.com>
2015-08-26 10:36:08 +0000

    project euler eerste probleem af

---
+++ ProjectEuler/problem1.cpp
@@ -0,0 +1,20 @@
+
+#include <iostream>
+#include <vector>
+#include <string>
+#include <cmath>
+using namespace std;
+
+int main(){
+    int totalscore = 0;
+    for (int i = 1; i < 334; i++){
+        totalscore += 3*i;
+    }
+    for (int i = 1; i < 200; i++){
+        if (5*i % 3 == 0)
+            continue;
+        totalscore += 5*i;
+    }
+    cout << totalscore;
+    return 0;
+}
